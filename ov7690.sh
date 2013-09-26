13c18d2c98666ba35e5aa36a983c6dc2c6611816
cjenkin2 <chris.jenkins@genesi-usa.com>
2013-09-26 12:17:59 -0500

    Independent register get/set for greater flexibility

--- ov7690.sh
+++ ov7690.sh
@@ -8,11 +8,12 @@ i2c_ov7690_set="sudo i2cset -f -y -r $I2C_BUS $OV7690_ADDR"
 
 # camera control defs
 # TODO bad practice - these two should be combined into one data structure
-OV7690_CONTROLS="AGC AEC AWB"
+OV7690_GROUPS="AGC AEC AWB"
 AGC="0x13 0x00 0x15"
 
 # script commands
-CMDS="get set help"
+GROUP_CMDS="group_get group_set"
+CMDS="get set help $GROUP_CMD"
 
 # helper functions
 unpack()
@@ -25,20 +26,31 @@ num_regs_for_control()
 	echo $(unpack $1) |  wc -w
 }
 
-# proper script functions
-usage()
+get_addr()
 {
-	echo "usage: $0 [control] [cmd] [vals...]"
-	echo "where	\"control\" is one of: $OV7690_CONTROLS"
-	echo "	\"cmd\" is one of: $CMDS"
-	echo "	vals... is a list of hexadecimal values (0x00 to 0xFF)"
+	echo $1 | cut -d= -f1
 }
 
+get_val()
+{
+	echo $1 | cut -d= -f2
+}
 
+# proper script functions
+#
+usage()
+{
+	echo "usage: $0 [group_cmd] [group] [val...]"
+	echo "       $0 get [addr...]"
+	echo "       $0 set [addr=val...]"
+	echo "where	\"group\" is one of: $OV7690_GROUPS"
+	echo "	\"group_cmd\" is one of: $GROUP_CMDS"
+	echo "	val... and addr... are lists of hexadecimal values (0x00 to 0xFF)"
+}
 
 get_regs()
 {
-	local REGS=$(unpack $1) # TODO HACK!
+	local REGS="$@"
 	for REG in $REGS
 	do
 		echo "$REG:	$(${i2c_ov7690_get} $REG)"
@@ -46,6 +58,22 @@ get_regs()
 }
 
 set_regs()
+{
+	local ADDR_VAL_PAIRS="$@"
+	for PAIR in $ADDR_VAL_PAIRS
+	do
+		local REG=$(get_addr $PAIR)
+		local VAL=$(get_val $PAIR)
+		echo "$REG:	$(${i2c_ov7690_set} $REG $VAL)"
+	done
+}
+
+get_regs_in_group()
+{
+	get_regs $(unpack $1)
+}
+
+set_regs_in_group()
 {
 	local REGS=$(unpack $1) # TODO HACK!
 	for REG in $REGS
@@ -61,24 +89,29 @@ if [ $# -lt 2 ]; then
 	exit 65 # bad params
 fi
 
-OV7690_CONTROL=$1
-CMD=$2
+CMD=$1
 
 case $CMD in
  help)
-	usage; echo ""
-	echo "	Registers for $OV7690_CONTROL: $(eval echo \${$OV7690_CONTROL})"
+	usage
 	;;
  get)
-	get_regs $OV7690_CONTROL
+	shift; get_regs "$@" # I hate this language
 	;;
  set)
-	shift 2
-	if [ $# -eq $(num_regs_for_control $OV7690_CONTROL) ]; then
-		set_regs $OV7690_CONTROL $@
+	shift; set_regs "$@" # I hate this language
+	;;
+ group_get)
+	OV7690_GROUP=$2
+	get_regs_in_group $OV7690_GROUP
+	;;
+ group_set)
+	OV7690_GROUP=$2; shift 2 # I hate this language
+	if [ $# -eq $(num_regs_for_control $OV7690_GROUP) ]; then
+		set_regs_in_group $OV7690_GROUP $@
 	else
-		echo "Error: Wrong number of params for control: $OV7690_CONTROL"
-		$0 $OV7690_CONTROL help
+		echo "Error: Wrong number of params for control: $OV7690_GROUP"
+		$0 $OV7690_GROUP help
 	fi
 	;;
 esac
